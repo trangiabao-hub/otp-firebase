4ac538e13829252e7424f866605903641f4d7f98
Tran Gia Bao <giabao@Trans-MacBook-Pro.local>
2022-09-06 16:36:56 +0700

    create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  auth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  confirmationResult,
+} from "./firebase/config";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 function App() {
+  const [code, setCode] = useState("");
+  const [phone, setPhone] = useState("");
+  const [confirmationResult, setConfirmationResult] = useState();
+
+  const [isShowVerify, setShowVerify] = useState(false);
+
+  // window.recaptchaVerifier = new RecaptchaVerifier(
+  //   "recaptcha-container",
+  //   {},
+  //   auth
+  // );
+
+  const sendOtp = () => {
+    const appVerifier = new RecaptchaVerifier(
+      "sign-in-button",
+      {
+        size: "invisible",
+        callback: (response) => {
+          // reCAPTCHA solved, allow signInWithPhoneNumber.
+          // onSignInSubmit();
+          Swal.fire(
+            "Good job!",
+            "Sent OTP Success, Please enter code! ",
+            "success"
+          );
+        },
+      },
+      auth
+    );
+    signInWithPhoneNumber(auth, phone, appVerifier)
+      .then((confirmationResult) => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        window.confirmationResult = confirmationResult;
+        setConfirmationResult(confirmationResult);
+        console.log("success");
+        // ...
+      })
+      .catch((error) => {
+        // Error; SMS not sent
+        // ...
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+        });
+      });
+  };
+
+  const verify = () => {
+    confirmationResult
+      .confirm(code)
+      .then((result) => {
+        // User signed in successfully.
+        const user = result.user;
+        console.log(user);
+        Swal.fire("Good job!", "Good OTP", "success");
+        // ...
+      })
+      .catch((error) => {
+        // User couldn't sign in (bad verification code?)
+        // ...
+        console.log(error);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Bad OTP",
+        });
+      });
+  };
+
+  const reset = () => {
+    setConfirmationResult();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginTop: "20px" }}>
+      {!confirmationResult ? (
+        <div className="phone">
+          <label htmlFor="phone">Phone: </label>
+          <input
+            type="phone"
+            onChange={(e) => {
+              setPhone(e.target.value);
+            }}
+            value={phone}
+          />
+          <button style={{ marginLeft: "10px" }} onClick={sendOtp}>
+            Sent OTP
+          </button>
+        </div>
+      ) : (
+        <div className="code" style={{ marginTop: "10px" }}>
+          <label htmlFor="phone">Code: </label>
+          <input
+            type="number"
+            value={code}
+            onChange={(e) => {
+              setCode(e.target.value);
+            }}
+          />
+          <button style={{ marginLeft: "10px" }} onClick={verify}>
+            Verify
+          </button>
+          <button style={{ marginLeft: "10px" }} onClick={reset}>
+            Reset
+          </button>
+        </div>
+      )}
+
+      <div id="sign-in-button" style={{ display: "none" }}></div>
     </div>
   );
 }
